ad6d253f2cdaee60430312038c11fb3aa790dafb
Ulanov DI <UlanovDI@sibelectro.com>
2026-09-10 08:08:26 +0700

    Разложил тесты по папкам

--- icarus-tests/makev.bat
+++ icarus-tests/makev.bat
@@ -3,6 +3,16 @@
 @REM pause
 
 
-iverilog -o qqq counter.v tcounter.v
-vvp qqq
+@REM iverilog -o qqq counter.v tcounter.v
+@REM vvp qqq
 
+@REM cd counter
+@REM iverilog -o counter.vvp counter.v tcounter.v
+@REM vvp counter.vvp
+@REM pause
+
+
+cd signal
+iverilog -o test -I./ -y./ bench.v
+@REM vvp test
+pause
